cbe272d496d853afbfbd68a377ccd0a6bae5adb9
Valentin Duffet <vavadu07@gmail.com>
2021-08-12 14:03:01 +0200

    Add custom fonts

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Welcome app sneakers</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+  let [fontsLoaded] = useFonts({
+    'Bebas Neue': require('./assets/fonts/BebasNeue-Regular.ttf'),
+    'Palette Mosaic': require('./assets/fonts/PaletteMosaic-Regular.ttf')
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text style={{fontFamily: "Palette Mosaic"}}>SNEAKERS APP</Text>
+        <Text style={{fontFamily: "Bebas Neue"}}>Welcome app sneakers</Text>
+      </View>
+    );
+  }
+
+  
 }
 
 const styles = StyleSheet.create({
