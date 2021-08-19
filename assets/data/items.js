0ca5cdd01cd17dd2a5c376db0af166e5f2dc44be
Valentin Duffet <vavadu07@gmail.com>
2021-08-19 14:18:39 +0200

    add detail page with data

--- assets/data/items.js
+++ assets/data/items.js
@@ -2,26 +2,29 @@ const items = [
     {
         id: 1,
         image: require('../Images/Air-Max-1-University-Red.jpg'),
-        title: "Air Max 1 University Red",
+        title: "Air Max 1 Anniversary Red",
         brand: "Nike",
-        price: 120,
-        condition: "New"
+        price: 257,
+        condition: "New",
+        colorway: "WHITE/UNIVERSITY RED-NEUTRAL GREY-BLACK"
     },
     {
         id: 2,
         image: require('../Images/Yeezy-350-Zebra.jpg'),
         title: "Yeezy 350 V2 Zebra",
         brand: "Adidas",
-        price: 120,
-        condition: "New"
+        price: 363,
+        condition: "New",
+        colorway: "WHITE/CORE"
     },
     {
         id: 3,
         image: require('../Images/Sacai.jpg'),
-        title: "Nike Waffle Sacai",
+        title: "LD Waffle sacai Black",
         brand: "Nike",
-        price: 120,
-        condition: "New"
+        price: 671,
+        condition: "New",
+        colorway: "BLACK/ANTHRACITE-WHITE"
     }
 ]
 
--- components/Detail.js
+++ components/Detail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity, Image } from 'react-native';
 
 import colors from "../assets/colors/colors"
 
@@ -17,6 +17,42 @@ export default function Detail({route, navigation}) {
                     <Ionicons name="chevron-back" size={26} color="white" />
                 </View>
             </TouchableOpacity>
+
+            <View style={styles.titleSection}>
+                <Text style={{fontFamily: "Bebas Neue", color: "white", fontSize: 24}}>{item.title}</Text>
+                <Text style={{fontFamily: "Bebas Neue", color: colors.text2, fontSize: 18}}>{item.brand}</Text>
+            </View>
+
+            <View style={styles.priceSection}>
+                <Text style={{fontFamily: "Bebas Neue", color: colors.primary, fontSize: 24}}>{item.price} €</Text>
+            </View>
+
+            <View style={styles.infoSection}>
+                <View style={styles.topInfoSection}>
+                    <Image source = {item.image}
+                        style = {{ 
+                            width: 300,
+                            height: 200,
+                            resizeMode: 'contain', 
+                            borderRadius: 50 }}
+                    />
+                </View>
+                <View style={styles.bottomInfoSection}>
+                    <View style={styles.conditionInfo}>
+                        <Text style={{fontFamily: "Bebas Neue", color: colors.text2, fontSize: 16}}>Condition</Text>
+                        <Text style={{fontFamily: "Bebas Neue", color: colors.primary, fontSize: 20}}>{item.condition}</Text>
+                    </View>
+                    <View style={styles.colorInfo}>
+                        <Text style={{fontFamily: "Bebas Neue", color: colors.text2, fontSize: 16}}>Colorway</Text>
+                        <Text style={{fontFamily: "Bebas Neue", color: colors.primary, fontSize: 20}}>{item.colorway}</Text>
+                    </View>
+                    <View style={styles.authInfo}>
+                        <Text style={{fontFamily: "Bebas Neue", color: colors.text2, fontSize: 16}}>100% Authentic</Text>
+                    </View>
+                </View>
+
+                
+            </View>
         </View>
     )
 }
@@ -33,5 +69,29 @@ const styles = StyleSheet.create({
         borderWidth: 2,
         width: 32,
         borderRadius: 10,
+    },
+    titleSection: {
+        marginTop: "10%",
+        paddingHorizontal: 20
+    },
+    priceSection : {
+        marginTop: "5%",
+        paddingHorizontal: 20
+    },
+    infoSection: {
+        marginTop: "10%",
+        paddingHorizontal: 20,
+    },
+    bottomInfoSection: {
+        marginTop: "15%"
+    },
+    conditionInfo: {
+        marginBottom: 20
+    },
+    colorInfo: {
+        marginBottom: 20
+    },
+    topInfoSection: {
+        alignItems: "center"
     }
 })
